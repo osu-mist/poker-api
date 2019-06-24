a104b7342ec4dac9054c6fb4cbb8264c7c188636
Zhuohong Gu <zhuohonggu@10-249-216-137.wireless.oregonstate.edu>
2019-06-24 14:04:06 -0700

    another change

--- api/v1/db/oracledb/member-dao.js
+++ api/v1/db/oracledb/member-dao.js
@@ -19,7 +19,6 @@ const getMembers = async () => {
   try {
     const rawMembers = await connection.execute("SELECT * FROM MEMBERS");
     const rawMembersRow = rawMembers.rows;
-    console.log(rawMembers);
     const serializedMembers = serializeMembers(rawMembersRow, endpointUri);
     return serializedMembers;
   } finally {
